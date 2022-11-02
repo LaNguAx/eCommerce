a67abf53c457c48835797822a71968113e6192a1
LaNguAx <itayakni@gmail.com>
2022-11-02 11:09:48 +0200

    changed the api from asos to fakestore api because asos one has limited requests per month

--- js/controller.js
+++ js/controller.js
@@ -1,32 +1,7 @@
-import { BrandsCategoryID } from './config.js';
 import * as model from './model.js';
 
-const controlProductList = async function () {
-  // await model.getCategoryProductList(BrandsCategoryID.lacoste);
-  // await model.getCategoryProductList(BrandsCategoryID.adidas, 500);
-  //NOTE -- LOAD THE NEXT RESULTS IN THE BACKGROUND WHEN IN PAGE 2, MEANING PAGE 3 WILL LOAD IN THE BACKGROUND WHEN PRESSING PAGE 2 WHEN UR ON PAGE 1. SO IT WONT BE FELT.
-  // await model.getCategoryProductList(BrandsCategoryID.boss);
-  // console.log(model.state.search.results);
-  // console.log('FINISHED LOADING');
-};
-
-controlProductList();
-// controlProductList();
-
-// model.getCategoryProductList(BrandsCategoryID.adidas, 120);
-
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '9cd03bd82dmshe9b a157b398e05dp13dea5jsnb28a46f65137',
-    'X-RapidAPI-Host': 'asos2.p.rapidapi.com',
-  },
-};
-
-fetch(
-  'https://asos2.p.rapidapi.com/categories/list?country=US&lang=en-US',
-  options
-)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+fetch('https://fakestoreapi.com/products')
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(element => console.log(element.category));
+  });
--- js/model.js
+++ js/model.js
@@ -1,9 +1,4 @@
-import {
-  API_OPTIONS,
-  API_URL_CATEGORIES_LIST,
-  API_URL_PRODUCTS_LIST,
-  RES_PER_PAGE,
-} from './config.js';
+import { RES_PER_PAGE } from './config.js';
 
 import { AJAX } from './helpers.js';
 
@@ -14,50 +9,3 @@ export const state = {
     resultsPerPage: RES_PER_PAGE,
   },
 };
-
-// fetch(API_URL_CATEGORY_LIST, API_OPTIONS)
-//   .then(response => response.json())
-//   .then(response => {
-//     console.log(response.brands[0]);
-//     console.log(
-//       response.brands[0].children.map(el => {
-//         return console.log(el.content.title, el.link.categoryId);
-//       })
-//     );
-//   })
-//   .catch(err => console.error(err));
-
-// fetch(API_URL_PRODUCT_LIST(BrandsCategoryID.adidas), API_OPTIONS)
-//   .then(response => response.json())
-//   .then(data => console.log(data));
-
-// fetch(API_URL_PRODUCTS_LIST(BrandsCategoryID.lacoste, 120), API_OPTIONS)
-//   .then(response => response.json())
-//   .then(response => console.log(response));
-
-export const getCategoryProductList = async function (brand, offset) {
-  const products = (({ products, ...o }) => products)(
-    await AJAX(API_URL_PRODUCTS_LIST(brand, offset), API_OPTIONS)
-  );
-  state.search.results = products.map(prod => {
-    const fixedObj = (({
-      brandName,
-      colour,
-      id,
-      imageUrl,
-      name,
-      price,
-      ...o
-    }) => ({ brandName, colour, id, imageUrl, name, price }))(prod);
-    return fixedObj;
-  });
-};
-
-// Get full product details
-// fetch(
-//   'https://asos2.p.rapidapi.com/products/v3/detail?id=202841816&lang=en-US&store=US&sizeSchema=US&currency=USD',
-//   API_OPTIONS
-// )
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));

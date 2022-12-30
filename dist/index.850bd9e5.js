3411aa9c626707a35fa5cbbd24b0ab553e0bd1d8
LaNguAx <itayakni@gmail.com>
2022-12-30 02:10:50 +0200

    fix sticky navigation

--- dist/index.850bd9e5.js
+++ dist/index.850bd9e5.js
@@ -2056,42 +2056,9 @@ class HeaderView {
         this.#stickyNavigation();
     }
     #stickyNavigation() {
-        //   const sectionHeroEl = document.querySelector('.header');
-        //   const obs = new IntersectionObserver(
-        //     function (entries) {
-        //       const ent = entries[0];
-        //       console.log(ent);
-        //       if (ent.isIntersecting === false) {
-        //         // sectionHeroEl.classList.add('sticky');
-        //         console.log('not intersecting');
-        //       }
-        //       if (ent.isIntersecting === true) {
-        //         // sectionHeroEl.classList.remove('sticky');
-        //         console.log('is intersecting');
-        //       }
-        //     },
-        //     {
-        //       // In the viewport
-        //       root: null,
-        //       threshold: 1,
-        //       rootMargin: `${sectionHeroEl.offsetHeight}px`,
-        //     }
-        //   );
-        //   obs.observe(sectionHeroEl);
-        // }
-        const creditsSection = document.querySelector(".credits-section");
-        const header = document.querySelector(".header");
-        const observer = new IntersectionObserver(function(entries) {
-            const [entry] = entries;
-            console.log(entry);
-            if (!entry.isIntersecting) header.classList.add("sticky");
-            if (entry.isIntersecting) header.classList.remove("sticky");
-        }, {
-            root: null,
-            threshold: 0,
-            rootMargin: ""
-        });
-        observer.observe(creditsSection);
+        const mainContainer = document.querySelector(".main-container");
+        const headerEl = document.querySelector(".header");
+        mainContainer.style.marginTop = `${headerEl.offsetHeight}px`;
     }
 }
 exports.default = new HeaderView();
